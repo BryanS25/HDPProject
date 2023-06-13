adb6a71106fe4f738072fb95968d4539f44da0b6
BryanS25 <90873633+BryanS25@users.noreply.github.com>
2023-06-13 11:07:38 -0600

    Delete AerolineaProject directory

--- AerolineaProject/aplicaciones/empleados/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Empleado
-#from .models import Administrador
-
-# Register your models here.
-admin.site.register(Empleado)
-#admin.site.register(Administrador)
